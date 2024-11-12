25bc34de389038478cb344e0fe6feb3f6b0431ed
ariella <ariella79070160@gmail.com>
2024-11-12 12:52:13 -0500

    Update vite.config.js

--- Product-reviews/vite.config.js
+++ Product-reviews/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ProductReviews/',
+  base: '/Product-reviews/',
   plugins: [react()],
 })
